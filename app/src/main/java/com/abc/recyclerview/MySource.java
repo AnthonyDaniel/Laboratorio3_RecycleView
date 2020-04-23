31a19d4555098949829156c3b826b9667e771cee
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2020-04-22 19:29:19 -0600

    Lab 3

---
+++ app/src/main/java/com/abc/recyclerview/MySource.java
@@ -0,0 +1,18 @@
+package com.abc.recyclerview;
+
+public class MySource extends Source {
+
+    /**
+     * @param title
+     * @param image
+     */
+
+    public MySource(String title, int image) {
+        super(title, image);
+    }
+
+    @Override
+    public String getTitle(){
+        return super.getTitle() + " Anthony"; // Parte de laboratorio
+    }
+}
